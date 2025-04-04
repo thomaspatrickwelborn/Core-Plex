c25573a7373e89e0fbbe59f3cfb80379d85105c1
Thomas Patrick Welborn <thomas.patrick.welborn@outlook.com>
2025-04-03 20:28:55 -0700

    Correction

--- distributement/core-plex.js
+++ distributement/core-plex.js
@@ -3,7 +3,6 @@ const defaultAccessor = ($target, $property) => {
   else { return $target[$property] }
 };
 const getAccessor = ($target, $property) => {
-  console.log($target, $property);
   if($property === undefined) { return $target }
   else { return $target.get($property) }
 };
